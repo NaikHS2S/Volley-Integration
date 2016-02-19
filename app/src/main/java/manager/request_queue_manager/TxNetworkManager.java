5bbc24fc512fa02cac202e7f316d138850baf4f1
hknaik988 <hknaik988@gmail.com>
2016-02-18 18:26:53 -0800

    UPDATED

---
+++ app/src/main/java/manager/request_queue_manager/TxNetworkManager.java
@@ -0,0 +1,61 @@
+package manager.request_queue_manager;
+
+import android.app.Application;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+import manager.utils.CustomImageCache;
+
+public class TxNetworkManager extends Application {
+
+    public static final String TAG = TxNetworkManager.class
+            .getSimpleName();
+
+    private RequestQueue mRequestQueue;
+    private ImageLoader mImageLoader;
+
+    private static TxNetworkManager mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+    }
+
+    public static synchronized TxNetworkManager getInstance() {
+
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return mRequestQueue;
+    }
+
+    public ImageLoader getImageLoader() {
+        getRequestQueue();
+        if (mImageLoader == null) {
+            mImageLoader = new ImageLoader(this.mRequestQueue,
+                    new CustomImageCache());
+        }
+        return this.mImageLoader;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        // set the default tag if tag is empty
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+}
---
+++ app/src/main/java/manager/response/UserContentMain.java
@@ -0,0 +1,34 @@
+package manager.response;
+
+import java.util.ArrayList;
+
+/**
+ * Created by computer on 2/18/2016.
+ */
+
+public class UserContentMain
+{
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    private String title;
+
+    private ArrayList<SingleUserInfo> rows;
+
+    public String getTitle ()
+    {
+        return title;
+    }
+
+    public ArrayList<SingleUserInfo>  getRows ()
+    {
+        return rows;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [title = "+title+", rows = "+rows+"]";
+    }
+}
