32f16f337fd68cdd2fa0c22770b4f91f96698bca
hknaik988 <hknaik988@gmail.com>
2016-02-22 19:59:58 -0800

    Unit test update

---
+++ app/src/androidTest/java/com/telstra/imagelist/ActivityTests.java
@@ -0,0 +1,46 @@
+package com.telstra.imagelist;
+
+/**
+ * Created by computer on 2/22/2016.
+ */
+
+import android.app.Instrumentation;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.ListView;
+
+import com.telstra.imagelist.ui.activity.TxMainActivity;
+
+public class ActivityTests extends
+        ActivityInstrumentationTestCase2<TxMainActivity> {
+    private TxMainActivity mTxMainActivity;
+    private Instrumentation mInstrumentation;
+    ListView list;
+
+    public ActivityTests() {
+        super(TxMainActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        setActivityInitialTouchMode(true);
+        mInstrumentation = getInstrumentation();
+        mTxMainActivity = getActivity();
+        list = (ListView) mTxMainActivity.findViewById(R.id.list);
+    }
+
+    public void testLoadsTxMainActivity() {
+        // Set up Activity Monitor
+        Instrumentation.ActivityMonitor mTxActivityMonitor =
+                mInstrumentation.addMonitor(TxMainActivity.class.getName(),
+                        null, false);
+
+        assertNotNull("The list was not loaded", list);
+
+        getInstrumentation().waitForIdleSync();
+
+        // Remove the Activity Monitor
+        getInstrumentation().removeMonitor(mTxActivityMonitor);
+    }
+
+}
