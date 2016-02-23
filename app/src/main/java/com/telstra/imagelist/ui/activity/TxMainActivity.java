6030fb663c5dfcde088a7fd3c7b217352bba4d68
hknaik988 <hknaik988@gmail.com>
2016-02-22 18:41:18 -0800

    Removed toolbar

--- app/src/main/java/com/telstra/imagelist/ui/activity/TxMainActivity.java
+++ app/src/main/java/com/telstra/imagelist/ui/activity/TxMainActivity.java
@@ -47,8 +47,6 @@ public class TxMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tx_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
